92cbf980b28ddd668d255f98b0cb92b97c9bfe5e
Andrew Zenk <azenk@umn.edu>
2018-03-21 15:14:24 -0500

    Fix various bugs

--- iputils.go
+++ iputils.go
@@ -1,30 +1,43 @@
 package iputils
 
 import (
+	"errors"
 	"net"
 )
 
-func SetBits(ip net.IP, bits uint64, startOffset, setWidth int) (net.IP, error) {
-	var newIP net.IP
+var (
+	ErrOutOfRange = errors.New("specified offset and/or width are invalid")
+)
+
+// SetBits sets the bits starting at startOffset and ending at startOffset + setWidth
+// to the bits specified.
+func SetBits(ip net.IP, bits uint64, startOffset, setWidth uint) (net.IP, error) {
+	newIP := make([]byte, 16)
 
 	if ip.To4() != nil {
-		newIP = net.IPv4zero
+		copy(newIP, net.IPv4zero)
 		startOffset = startOffset + 96
 	} else {
-		newIP = net.IPv6zero
+		copy(newIP, net.IPv6zero)
 	}
+
+	if startOffset+setWidth > 128 {
+		return newIP, ErrOutOfRange
+	}
+
+	bits &= ^uint64(0) >> uint(64-setWidth)
 
 	for i, b := range ip {
 		newIP[i] = b
-		if (i+1)*8 > startOffset && i*8 <= startOffset+setWidth {
-			shift := startOffset + setWidth - (i+1)*8
+		if (i+1)*8 > int(startOffset) && i*8 < int(startOffset+setWidth) {
+			shift := int(startOffset+setWidth) - (i+1)*8
 			var t, mask byte
 			if shift > 0 {
-				t = byte(bits >> uint(shift) & 0x00ff)
-				mask = byte(0x0ff >> uint(shift) & 0x0ff)
+				mask = byte(^uint64(0) >> uint(shift) & 0x0ff)
+				t = byte(bits>>uint(shift)) & mask
 			} else {
-				t = byte(bits << uint(-1*shift) & 0x00ff)
-				mask = byte(0x0ff << uint(-1*shift) & 0x0ff)
+				mask = byte(^uint64(0) << uint(-1*shift) & 0x0ff)
+				t = byte(bits<<uint(-1*shift)) & mask
 			}
 			newIP[i] = b&mask | t
 		}
