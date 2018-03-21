95423b6bbb3054e3c826de9646f5aa590524f844
Andrew Zenk <azenk@umn.edu>
2018-03-21 14:02:16 -0500

    Initial Commit

--- iputils.go
+++ iputils.go
@@ -1,1 +1,33 @@
 package iputils
+
+import (
+	"net"
+)
+
+func SetBits(ip net.IP, bits uint64, startOffset, setWidth int) (net.IP, error) {
+	var newIP net.IP
+
+	if ip.To4() != nil {
+		newIP = net.IPv4zero
+		startOffset = startOffset + 96
+	} else {
+		newIP = net.IPv6zero
+	}
+
+	for i, b := range ip {
+		newIP[i] = b
+		if (i+1)*8 > startOffset && i*8 <= startOffset+setWidth {
+			shift := startOffset + setWidth - (i+1)*8
+			var t, mask byte
+			if shift > 0 {
+				t = byte(bits >> uint(shift) & 0x00ff)
+				mask = byte(0x0ff >> uint(shift) & 0x0ff)
+			} else {
+				t = byte(bits << uint(-1*shift) & 0x00ff)
+				mask = byte(0x0ff << uint(-1*shift) & 0x0ff)
+			}
+			newIP[i] = b&mask | t
+		}
+	}
+	return newIP, nil
+}
--- iputils_test.go
+++ iputils_test.go
@@ -1,1 +1,35 @@
 package iputils
+
+import (
+	"net"
+	"testing"
+)
+
+func TestSetBits(t *testing.T) {
+	type ipSetTestCase struct {
+		BaseIP     net.IP
+		Bits       uint64
+		Offset     int
+		Width      int
+		ExpectedIP net.IP
+	}
+
+	testCases := []ipSetTestCase{
+		ipSetTestCase{net.ParseIP("10.0.0.0"), 0xff, 16, 8, net.ParseIP("10.0.255.0")},
+		ipSetTestCase{net.ParseIP("10.0.15.0"), 0xff, 12, 8, net.ParseIP("10.15.240.0")},
+		ipSetTestCase{net.ParseIP("2001:db8:0:1::"), 0xffffffff, 64, 32, net.ParseIP("2001:db8:0:1:ffff:ffff::")},
+		ipSetTestCase{net.ParseIP("2001:db8:0:1::"), 0xffffffff, 66, 32, net.ParseIP("2001:db8:0:1:3fff:ffff:c000:0")},
+		ipSetTestCase{net.ParseIP("2001:db8:0:1::"), 0xf3f2f1f0, 72, 24, net.ParseIP("2001:db8:0:1:00f2:f1f0::")},
+	}
+
+	for _, testCase := range testCases {
+		newIP, err := SetBits(testCase.BaseIP, testCase.Bits, testCase.Offset, testCase.Width)
+		if err != nil {
+			t.Fatalf("unable to set bits: %v", err)
+		}
+
+		if newIP.String() != testCase.ExpectedIP.String() {
+			t.Errorf("Expected IP: %s, Got: %s", testCase.ExpectedIP.String(), newIP.String())
+		}
+	}
+}
