2bfbac65138c9374e574e516fa6faba4439dbd7e
Andrew Zenk <azenk@umn.edu>
2018-03-21 15:14:00 -0500

    Add support for testing expected errors

--- iputils_test.go
+++ iputils_test.go
@@ -7,25 +7,29 @@ import (
 
 func TestSetBits(t *testing.T) {
 	type ipSetTestCase struct {
-		BaseIP     net.IP
-		Bits       uint64
-		Offset     int
-		Width      int
-		ExpectedIP net.IP
+		BaseIP      net.IP
+		Bits        uint64
+		Offset      uint
+		Width       uint
+		ExpectedIP  net.IP
+		ExpectedErr error
 	}
 
 	testCases := []ipSetTestCase{
-		ipSetTestCase{net.ParseIP("10.0.0.0"), 0xff, 16, 8, net.ParseIP("10.0.255.0")},
-		ipSetTestCase{net.ParseIP("10.0.15.0"), 0xff, 12, 8, net.ParseIP("10.15.240.0")},
-		ipSetTestCase{net.ParseIP("2001:db8:0:1::"), 0xffffffff, 64, 32, net.ParseIP("2001:db8:0:1:ffff:ffff::")},
-		ipSetTestCase{net.ParseIP("2001:db8:0:1::"), 0xffffffff, 66, 32, net.ParseIP("2001:db8:0:1:3fff:ffff:c000:0")},
-		ipSetTestCase{net.ParseIP("2001:db8:0:1::"), 0xf3f2f1f0, 72, 24, net.ParseIP("2001:db8:0:1:00f2:f1f0::")},
+		ipSetTestCase{net.ParseIP("10.0.0.0"), 0xff, 16, 8, net.ParseIP("10.0.255.0"), nil},
+		ipSetTestCase{net.ParseIP("10.0.15.0"), 0xff, 12, 8, net.ParseIP("10.15.240.0"), nil},
+		ipSetTestCase{net.ParseIP("2001:db8:0:1::"), 0xffffffff, 64, 32, net.ParseIP("2001:db8:0:1:ffff:ffff::"), nil},
+		ipSetTestCase{net.ParseIP("2001:db8:0:1::"), 0xffffffff, 66, 32, net.ParseIP("2001:db8:0:1:3fff:ffff:c000:0"), nil},
+		ipSetTestCase{net.ParseIP("2001:db8:0:1::"), 0xf3f2f1f0, 72, 24, net.ParseIP("2001:db8:0:1:00f2:f1f0::"), nil},
+		ipSetTestCase{net.ParseIP("2001:db8:0:6:683f:ea00::"), ^uint64(0), 92, 36, net.ParseIP("2001:db8:0:6:683f:ea0f:ffff:ffff"), nil},
+		ipSetTestCase{net.ParseIP("2001:db8:0:1::"), 0xf3f2f1f0, 100, 32, net.ParseIP("::"), ErrOutOfRange},
+		ipSetTestCase{net.ParseIP("10.0.0.0"), 0xff, 25, 8, net.ParseIP("0.0.0.0"), ErrOutOfRange},
 	}
 
 	for _, testCase := range testCases {
 		newIP, err := SetBits(testCase.BaseIP, testCase.Bits, testCase.Offset, testCase.Width)
-		if err != nil {
-			t.Fatalf("unable to set bits: %v", err)
+		if err != testCase.ExpectedErr {
+			t.Errorf("Expected err %v, got: %v", testCase.ExpectedErr, err)
 		}
 
 		if newIP.String() != testCase.ExpectedIP.String() {
